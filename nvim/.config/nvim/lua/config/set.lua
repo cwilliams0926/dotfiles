a44484b37615965b2e06d52e2dbb2f38300d14ce
cwilliams0926 <coopwilliams03@hotmail.com>
2026-04-30 03:33:25 -0400

    Configure rofi, change wallpaper, and other stuff

--- nvim/.config/nvim/lua/config/set.lua
+++ nvim/.config/nvim/lua/config/set.lua
@@ -1,5 +1,5 @@
 vim.opt.number = true
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
@@ -9,13 +9,13 @@ vim.opt.expandtab = true
 
 -- html tab width fix
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "html",
-    callback = function()
-        vim.opt_local.tabstop = 2
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.softtabstop = 2
-        vim.opt_local.expandtab = true
-    end
+  pattern = "html",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
 })
 
 vim.opt.wrap = false
