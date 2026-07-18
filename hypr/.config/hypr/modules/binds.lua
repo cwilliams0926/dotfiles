d5bd0d8b2bf1c9ac88fec279077275e14249bcbe
cwilliams0926 <coopwilliams03@hotmail.com>
2026-07-17 21:14:48 -0400

    Added battery saving bind

--- hypr/.config/hypr/modules/binds.lua
+++ hypr/.config/hypr/modules/binds.lua
@@ -141,3 +141,36 @@ hl.bind(mainMod .. " + f", hl.dsp.window.fullscreen({ "fullscreen", toggle }))
 -- Move window
 
 hl.bind(mainMod .. " + n", hl.dsp.window.swap({ next = true }))
+
+-- Toggle waybar
+hl.bind("SUPER + m", hl.dsp.exec_cmd("pkill waybar || waybar &"))
+
+-- The "no fun" bind
+hl.bind("SUPER + F1", function()
+  local game_mode = (hl.get_config("animations.enabled") == false)
+  hl.exec_cmd("pkill waybar && waybar -s ~/.config/waybar/minimal.css -c ~/.config/waybar/minimal-config.jsonc &")
+
+  if game_mode then
+    hl.exec_cmd("hyprctl reload")
+    return
+  end
+
+  hl.config({
+    general = {
+      gaps_in = 0,
+      gaps_out = 0, -- Disable gaps
+      border_size = 0,
+    },
+
+    animations = {
+      enabled = false, -- Disable animations
+    },
+
+    -- Disable blur, shadow and window rounding
+    decoration = {
+      shadow = { enabled = false },
+      blur = { enabled = false },
+      rounding = 0,
+    },
+  })
+end)
