ab9ca67bd6ef79699ac527a99a6fcb9b47ab06ab
cwilliams0926 <coopwilliams03@hotmail.com>
2026-07-22 18:00:35 -0400

    Add e-ink and nord to each custom color directory

---
+++ hypr/.config/hypr/colors/custom/e-ink.lua
@@ -0,0 +1,42 @@
+---@module 'hl'
+
+local bg0 = "#333333"
+local bg1 = "#474747"
+local bg2 = "#4a4a4a"
+local bg3 = "#545454"
+local bg4 = "#5e5e5e"
+
+local fg = "#cccccc"
+
+local red = "#fb4934"
+local orange = "#fe8019"
+local yellow = "#fabd2f"
+local green = "#b8bb26"
+local aqua = "#8ec07c"
+local blue = "#83a598"
+local purple = "#d3869b"
+local pink = "#d3869b"
+
+local grey0 = "#686868"
+local grey1 = "#727272"
+local grey2 = "#7c7c7c"
+
+return {
+  bg0 = bg0,
+  bg1 = bg1,
+  bg2 = bg2,
+  bg3 = bg3,
+  bg4 = bg4,
+  fg = fg,
+  red = red,
+  orange = orange,
+  yellow = yellow,
+  green = green,
+  aqua = aqua,
+  blue = blue,
+  purple = purple,
+  pink = pink,
+  grey0 = grey0,
+  grey1 = grey1,
+  grey2 = grey2,
+}
