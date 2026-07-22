6115ba21095b5f77323b4dce88e9ab625a81135f
cwilliams0926 <coopwilliams03@hotmail.com>
2026-07-21 23:54:47 -0400

    Configure tmux

--- hypr/.config/hypr/modules/decorations/Boxed.lua
+++ hypr/.config/hypr/modules/decorations/Boxed.lua
@@ -3,15 +3,15 @@
 local colors = require("colors.colors")
 
 hl.config({
-  general = {
-    border_size = 2,
-    ["col.active_border"] = colors.bg5,
-    ["col.inactive_border"] = colors.bg1,
-  },
+	general = {
+		border_size = 2,
+		["col.active_border"] = colors.grey0,
+		["col.inactive_border"] = colors.bg1,
+	},
 })
 
 hl.config({
-  decoration = {
-    rounding = 0,
-  },
+	decoration = {
+		rounding = 0,
+	},
 })
