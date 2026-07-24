b549e1aaa34d697498b565c980a950cd86b441da
cwilliams0926 <coopwilliams03@hotmail.com>
2026-07-24 16:56:14 -0400

    Add tokyonight colorscheme

---
+++ hypr/.config/hypr/colors/custom/tokyonight.lua
@@ -0,0 +1,42 @@
+---@module 'hl'
+
+local bg0 = "#1e2030"
+local bg1 = "#222436"
+local bg2 = "#2f334d"
+local bg3 = "#3b4261"
+local bg4 = "#444a73"
+
+local fg = "#c8d3f5"
+
+local red = "#ff757f"
+local orange = "#ff966c"
+local yellow = "#ffc777"
+local green = "#c3e88d"
+local aqua = "#86e1fc"
+local blue = "#82aaff"
+local purple = "#c099ff"
+local pink = "#fca7ea"
+
+local grey0 = "#545c7e"
+local grey1 = "#737aa2"
+local grey2 = "#828bb8"
+
+return {
+  bg0 = bg0,
+  bg1 = bg1,
+  bg2 = bg2,
+  bg3 = bg3,
+  bg4 = bg4,
+  fg = fg,
+  red = red,
+  orange = orange,
+  yellow = yellow,
+  green = green,
+  aqua = aqua,
+  blue = blue,
+  purple = purple,
+  pink = pink,
+  grey0 = grey0,
+  grey1 = grey1,
+  grey2 = grey2,
+}
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -101,10 +101,34 @@ return {
     config = function()
       require("e-ink").setup()
       vim.opt.background = "light"
+      vim.cmd.colorscheme("e-ink")
       local set_hl = vim.api.nvim_set_hl
       local mono = require("e-ink.palette").mono()
 
       set_hl(0, "Normal", { fg = mono[12], bg = "NONE" })
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    cond = theme == "tokyonight",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "moon",
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true, bold = true },
+          functions = { bold = true },
+          variables = {},
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
