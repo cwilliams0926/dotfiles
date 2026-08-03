bde860204dfb2a79a55a11e72eb349bf897ba9d7
cwilliams0926 <coopwilliams03@hotmail.com>
2026-08-03 17:10:22 -0400

    Add kanagawa to theme switcher pipeline

---
+++ hypr/.config/hypr/colors/custom/kanagawa.lua
@@ -0,0 +1,42 @@
+---@module 'hl'
+
+local bg0 = "#16161d"
+local bg1 = "#1f1f28"
+local bg2 = "#2a2a37"
+local bg3 = "#363646"
+local bg4 = "#2d4f67"
+
+local fg = "#dcd7ba"
+
+local red = "#e82424"
+local orange = "#ffa066"
+local yellow = "#e6c384"
+local green = "#98bb6c"
+local aqua = "#6a9589"
+local blue = "#7e9cd8"
+local purple = "#957fb8"
+local pink = "#d27e99"
+
+local grey0 = "#727169"
+local grey1 = "#54546d"
+local grey2 = "#c8c093"
+
+return {
+	bg0 = bg0,
+	bg1 = bg1,
+	bg2 = bg2,
+	bg3 = bg3,
+	bg4 = bg4,
+	fg = fg,
+	red = red,
+	orange = orange,
+	yellow = yellow,
+	green = green,
+	aqua = aqua,
+	blue = blue,
+	purple = purple,
+	pink = pink,
+	grey0 = grey0,
+	grey1 = grey1,
+	grey2 = grey2,
+}
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,134 +1,157 @@
 local theme = require("config.theme").current()
 
 return {
-  {
-    "sainnhe/gruvbox-material",
-    cond = theme == "gruvbox-material",
-    priority = 1000,
-    config = function()
-      vim.o.background = "dark"
-      vim.g.gruvbox_material_background = "soft"
-      vim.g.gruvbox_material_transparent_background = 1
-      vim.g.gruvbox_material_enable_bold = 1
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.cmd.colorscheme("gruvbox-material")
-      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
-    end,
-  },
+	{
+		"sainnhe/gruvbox-material",
+		cond = theme == "gruvbox-material",
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark"
+			vim.g.gruvbox_material_background = "soft"
+			vim.g.gruvbox_material_transparent_background = 1
+			vim.g.gruvbox_material_enable_bold = 1
+			vim.g.gruvbox_material_enable_italic = 1
+			vim.cmd.colorscheme("gruvbox-material")
+			vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+			vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+		end,
+	},
 
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    cond = theme == "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- dark variant
-        transparent_background = true,
-        styles = {
-          comments = { "italic" },
-          keywords = { "italic" },
-          functions = { "bold" },
-        },
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		cond = theme == "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- dark variant
+				transparent_background = true,
+				styles = {
+					comments = { "italic" },
+					keywords = { "italic" },
+					functions = { "bold" },
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 
-  {
-    "sainnhe/everforest",
-    cond = theme == "everforest",
-    priority = 1000,
-    config = function()
-      vim.o.background = "dark"
-      vim.g.everforest_background = "soft"
-      vim.g.everforest_transparent_background = 1
-      vim.g.everforest_enable_italic = 1
-      vim.g.everforest_diagnostic_text_highlight = 1
-      vim.g.everforest_diagnostic_virtual_text = "colored"
-      vim.cmd.colorscheme("everforest")
-      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
-    end,
-  },
+	{
+		"sainnhe/everforest",
+		cond = theme == "everforest",
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark"
+			vim.g.everforest_background = "soft"
+			vim.g.everforest_transparent_background = 1
+			vim.g.everforest_enable_italic = 1
+			vim.g.everforest_diagnostic_text_highlight = 1
+			vim.g.everforest_diagnostic_virtual_text = "colored"
+			vim.cmd.colorscheme("everforest")
+			vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+			vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+		end,
+	},
 
-  {
-    "ellisonleao/gruvbox.nvim",
-    cond = theme == "gruvbox",
-    priority = 1000,
-    config = function()
-      vim.o.background = "dark"
-      require("gruvbox").setup({
-        contrast = "soft",
-        transparent_mode = true,
-        italic = {
-          strings = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        bold = true,
-      })
-      vim.cmd.colorscheme("gruvbox")
-      vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-      vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
-    end,
-  },
+	{
+		"ellisonleao/gruvbox.nvim",
+		cond = theme == "gruvbox",
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark"
+			require("gruvbox").setup({
+				contrast = "soft",
+				transparent_mode = true,
+				italic = {
+					strings = true,
+					comments = true,
+					operators = false,
+					folds = true,
+				},
+				bold = true,
+			})
+			vim.cmd.colorscheme("gruvbox")
+			vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+			vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+		end,
+	},
 
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    cond = theme == "rosepine",
-    priority = 1000,
-    config = function()
-      require("rose-pine").setup({
-        variant = "main", -- dark variant ("main", "moon", "dawn" is the light one)
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = true,
-        },
-      })
-      vim.cmd.colorscheme("rose-pine")
-    end,
-  },
-  {
-    "e-ink-colorscheme/e-ink.nvim",
-    name = "e-ink",
-    cond = theme == "e-ink",
-    priority = 1000,
-    config = function()
-      require("e-ink").setup()
-      vim.opt.background = "light"
-      vim.cmd.colorscheme("e-ink")
-      local set_hl = vim.api.nvim_set_hl
-      local mono = require("e-ink.palette").mono()
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		cond = theme == "rosepine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				variant = "main", -- dark variant ("main", "moon", "dawn" is the light one)
+				styles = {
+					bold = true,
+					italic = true,
+					transparency = true,
+				},
+			})
+			vim.cmd.colorscheme("rose-pine")
+		end,
+	},
+	{
+		"e-ink-colorscheme/e-ink.nvim",
+		name = "e-ink",
+		cond = theme == "e-ink",
+		priority = 1000,
+		config = function()
+			require("e-ink").setup()
+			vim.opt.background = "light"
+			vim.cmd.colorscheme("e-ink")
+			local set_hl = vim.api.nvim_set_hl
+			local mono = require("e-ink.palette").mono()
 
-      set_hl(0, "Normal", { fg = mono[12], bg = "NONE" })
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    cond = theme == "tokyonight",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("tokyonight").setup({
-        style = "moon",
-        transparent = true,
-        terminal_colors = true,
-        styles = {
-          comments = { italic = true },
-          keywords = { italic = true, bold = true },
-          functions = { bold = true },
-          variables = {},
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      })
-      vim.cmd.colorscheme("tokyonight")
-    end,
-  },
+			set_hl(0, "Normal", { fg = mono[12], bg = "NONE" })
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		cond = theme == "tokyonight",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				style = "moon",
+				transparent = true,
+				terminal_colors = true,
+				styles = {
+					comments = { italic = true },
+					keywords = { italic = true, bold = true },
+					functions = { bold = true },
+					variables = {},
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+			})
+			vim.cmd.colorscheme("tokyonight")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		cond = theme == "kanagawa",
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				theme = "wave",
+				transparent = true,
+				dimInactive = true,
+				terminalColors = true,
+				colors = {
+					theme = {
+						wave = {
+							ui = { bg_gutter = "none" },
+						},
+					},
+				},
+			})
+			vim.cmd.colorscheme("kanagawa")
+			vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+			vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+		end,
+	},
 }
